8339090b8b5366559541eac1df951fb6c6fadceb
Danilo Horta <danilo.horta@gmail.com>
2017-02-02 16:59:56 +0000

    automatic

--- limix_genetics/_qqplot.py
+++ limix_genetics/_qqplot.py
@@ -23,7 +23,10 @@ def qqplot(df,
         tools = []
 
     if figure is None:
-        figure = bokeh.plotting.figure(tools=tools)
+        figure = bokeh.plotting.figure(
+            tools=tools,
+            x_axis_label="theoretical -log$_{10}$(p-value)",
+            y_axis_label="observed -log$_{10}$(p-value)")
 
     labels = _labels(df)
     colors = _colors(colors, labels)
