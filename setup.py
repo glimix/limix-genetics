9c69a59a61e030243a9f865a7f7ae8842859eaee
Danilo Horta <danilo.horta@gmail.com>
2017-02-14 13:26:48 +0000

    mvnorm cdf release

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def setup_package():
 
     metadata = dict(
         name='limix-genetics',
-        version='0.1.6.dev2',
+        version='0.1.6',
         maintainer="Limix Developers",
         maintainer_email="horta@ebi.ac.uk",
         license="MIT",
